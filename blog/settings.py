f7029133ec2686b187ecc9f70ba1b6a4334a03d4
Vikrantpaama <kumarvikrant311@gmail.com>
2022-10-12 18:54:35 +0530

    g

--- blog/settings.py
+++ blog/settings.py
@@ -1,7 +1,8 @@
 import os
-
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
